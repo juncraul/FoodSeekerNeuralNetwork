e0779d339f8e323156f0f3eb29bd7db9a3f97bed
Raul <juncraul@yahoo.com>
2017-02-15 23:59:40 +0000

    Agents appeared on the screen

--- FoodSeekerNeuralNetwork/FoodSeekerNeuralNetwork/Form1.cs
+++ FoodSeekerNeuralNetwork/FoodSeekerNeuralNetwork/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Population;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,51 @@ namespace FoodSeekerNeuralNetwork
         public Form1()
         {
             InitializeComponent();
+        }
+
+        Graphics graphics;
+        Bitmap bitmap;
+        Random random;
+        List<Agent> agents;
+        Timer timerForDrawing;
+        Timer timerForLogic;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            random = new Random();
+            bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(bitmap);
+            agents = new List<Agent>();
+            timerForDrawing = new Timer();
+            timerForDrawing.Interval = 200;
+            timerForDrawing.Tick += TimerForDrawing_Tick;
+            timerForLogic = new Timer();
+            timerForLogic.Interval = 50;
+            timerForLogic.Tick += TimerForLogic_Tick;
+
+
+            for (int i = 0; i < 10; i ++)
+            {
+                agents.Add(new Agent(new Point(random.Next() % pictureBox1.Width, random.Next() % pictureBox1.Height)));
+            }
+            timerForDrawing.Start();
+        }
+
+        private void TimerForLogic_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void TimerForDrawing_Tick(object sender, EventArgs e)
+        {
+            SolidBrush brush = new SolidBrush(Color.Wheat);
+            graphics.FillRectangle(brush, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+            foreach(Agent a in agents)
+            {
+                a.Draw(graphics);
+            }
+
+            pictureBox1.Image = bitmap;
         }
     }
 }
---
+++ FoodSeekerNeuralNetwork/Population/Agent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Population
+{
+    public class Agent
+    {
+        public double Health { get; set; }
+        public Point Position { get; set; }
+        public Size Size { get; set; }
+        public Color Color { get; set; }
+
+        public Agent(Point position)
+        {
+            Position = position;
+            Size = new Size(20, 20);
+            Color = Color.Red;
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            SolidBrush brush = new SolidBrush(Color);
+            graphics.FillEllipse(brush, new Rectangle(Position, Size));
+        }
+    }
+}
